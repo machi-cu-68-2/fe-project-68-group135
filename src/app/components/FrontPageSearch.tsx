fc8fb4315f3abccfc1651371e0a12dc8631c133e
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-03-20 17:51:34 +0700

    fix(ui): prevent redundant rules

--- src/app/components/FrontPageSearch.tsx
+++ src/app/components/FrontPageSearch.tsx
@@ -4,8 +4,8 @@
 export default function FrontPageSearch() {
   return (
     <div className="flex flex-col md:flex-row md:w-[42.5%] md:justify-between">
-      <input className="bg-white text-black px-[8px] py-[8px] text-xl rounded-md my-[4px] md:mr-[4px] md:my-[0px] md:flex-grow"></input>
-      <button className="bg-emerald-700 rounded-md px-[8px] py-[8px] text-xl">Search</button>
+      <input className="bg-white text-black p-[8px] text-xl rounded-md my-[4px] md:mr-[4px] md:my-[0px] md:flex-grow"></input>
+      <button className="bg-emerald-700 rounded-md p-[8px] text-xl">Search</button>
     </div>
   )
 }
