14b3d0f38bcd168bffa2f3f184e0912220970333
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-03-20 17:54:17 +0700

    fix(ui): fix banner on smaller devices (<md = 50vh, >=md = 30vh)

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -2,7 +2,7 @@ import FrontPageSearch from "./FrontPageSearch";
 
 export default function Banner() {
   return (
-    <div className="w-full bg-primary flex justify-center items-center h-[30vh]">
+    <div className="w-full bg-primary flex justify-center items-center h-[50vh] md:h-[30vh]">
       <div className="w-[var(--general-width)] text-white">
         <span className="text-4xl">Find your perfect stay!</span>
         <br />
