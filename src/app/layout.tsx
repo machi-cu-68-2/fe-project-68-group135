ea5f121f9b7b3053eae8bcdc718718a94a99c52e
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-03-20 17:50:45 +0700

    feat(ui): update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,20 @@
 import type { Metadata } from "next";
-import { Sarabun } from 'next/font/google';
+import { Sarabun } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header";
 
 const sarabun = Sarabun({
-  weight: ["400","700"],
-  subsets: ["latin","thai"],
-})
+  weight: ["400", "700"],
+  subsets: ["latin", "thai"]
+});
 
 export const metadata: Metadata = {
   title: "Group135 Frontend",
-  description: "someone changed the group name to something uncool :(",
+  description: "someone changed the group name to something uncool :("
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <head></head>
       <body className={sarabun.className}>
+        <Header />
         {children}
       </body>
     </html>
