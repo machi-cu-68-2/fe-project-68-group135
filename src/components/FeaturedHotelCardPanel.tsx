1508af9ebe0a464d28ec43186c6e94f3c00ed0ab
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-03-20 19:06:54 +0700

    feat(ui): add `FeaturedHotelCardPanel.tsx`

---
+++ src/components/FeaturedHotelCardPanel.tsx
@@ -0,0 +1,25 @@
+import HotelCard from "./HotelCard";
+
+export default function FeaturedHotelCardPanel() {
+  return (
+    <div className="w-full">
+      <div className="flex flex-row justify-between">
+        <HotelCard
+          name="mistertfy64's Hotel"
+          location="The Algebra Playground"
+          tel="1234567890"
+        />
+        <HotelCard
+          name="Meep2427's Hotel"
+          location="Dream Island, Goiky"
+          tel="2427242724"
+        />
+        <HotelCard
+          name="kaikubez's Hotel"
+          location="The Grasslands, Goiky"
+          tel="1111111111"
+        />
+      </div>
+    </div>
+  );
+}
