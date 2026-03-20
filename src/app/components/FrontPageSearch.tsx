51c8f483e004ae95d984f4491d2bc9b68599e60b
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2026-03-20 18:17:13 +0700

    fix(ui): change padding for search button

--- src/app/components/FrontPageSearch.tsx
+++ src/app/components/FrontPageSearch.tsx
@@ -4,7 +4,7 @@ export default function FrontPageSearch() {
   return (
     <div className="flex flex-col md:flex-row md:w-[42.5%] md:justify-between">
       <input className="bg-white text-black p-[8px] text-xl rounded-md my-[4px] md:mr-[8px] md:my-[0px] md:flex-grow"></input>
-      <button className="cursor-pointer bg-emerald-700 rounded-md p-[8px] text-xl">
+      <button className="cursor-pointer bg-emerald-700 rounded-md px-[16px] py-[8px] text-xl">
         Search
       </button>
     </div>
